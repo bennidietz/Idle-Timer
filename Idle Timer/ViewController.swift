b4db8f21948515ab44b890c4cc0af5eca3bb9f2c
Benni Dietz <b_diet05@uni-muenster.de>
2021-02-10 22:10:44 +0100

    Initial Commit

---
+++ Idle Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Idle Timer
+//
+//  Created by Benjamin Dietz on 10.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
