8cfa44b83302150f6e0ddd871eb1282bfde9ed5d
Benni Dietz <b_diet05@uni-muenster.de>
2021-02-14 23:53:41 +0100

    Final changes

--- Idle Timer/ViewController.swift
+++ Idle Timer/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainTreeImageView: UIImageView!
     @IBOutlet weak var widthConstraint: NSLayoutConstraint!
     @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var treeThumbnails: [UIImageView]!
     var timer: Timer?
     
     var currentTree = 1
     var timePassed = 0
+    var grownTrees = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +31,36 @@ class ViewController: UIViewController {
             widthConstraint.constant += widthConstraint.constant * 0.5
             mainTreeImageView.image = image
         } else if let image = UIImage(named: "Tree1") {
-            widthConstraint.constant = 100
+            if grownTrees == 5 {
+                resetApp()
+            } else {
+                treeThumbnails[grownTrees].alpha = 1.0
+                grownTrees += 1
+                widthConstraint.constant = 100
+                mainTreeImageView.image = image
+                currentTree = 1
+            }
+        }
+    }
+    
+    func resetApp() {
+        timer?.invalidate()
+        currentTree = 1
+        timePassed = 0
+        grownTrees = 0
+        if let image = UIImage(named: "Tree1") {
             mainTreeImageView.image = image
-            currentTree = 1
+            widthConstraint.constant = 100
+        }
+        actionButton.setTitle("START", for: .normal)
+        for treeImageView in treeThumbnails {
+            treeImageView.alpha = 0.4
         }
     }
     
     @objc func updateTime() {
         timePassed += 1
-        if timePassed % 2 == 0 {
+        if timePassed % 1 == 0 {
             updateTrees()
         }
     }
@@ -48,8 +71,7 @@ class ViewController: UIViewController {
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
             actionButton.setTitle("STOP", for: .normal) // control state
         case "STOP":
-            actionButton.setTitle("START", for: .normal)
-            timer?.invalidate()
+            resetApp()
         default:
             break
         }
